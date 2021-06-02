0733c59489c01732a1bd82d69b645fbd8cdc932e
Jyldyz Sultanbekova <sultanbekova.jyldyz@gmail.com>
2021-06-02 12:58:51 +0600

    error correction

--- app/src/main/java/kg/tutorialapp/gestures/MainActivity.kt
+++ app/src/main/java/kg/tutorialapp/gestures/MainActivity.kt
@@ -29,54 +29,54 @@ class MainActivity() : AppCompatActivity(), GestureDetector.OnGestureListener, G
     }
 
     override fun onSingleTapConfirmed(e: MotionEvent?): Boolean {
-        text2?.text = "Обычное одиночное нажатие на экран"
-        text1!!.text = "onLongPress$e"
+        text2?.text = "Обычное одиночное нажатие на экран"          //+
+        text1!!.text = ("onSingleTapConfirmed :" + e.toString())
         return true
     }
 
-    override fun onDoubleTap(e: MotionEvent?): Boolean {
+    override fun onDoubleTap(e: MotionEvent?): Boolean {          //
         text2?.text = "Двойное нажатие на экран"
-        text1!!.text = "onLongPress$e"
+        text1!!.text = ("onDoubleTap :" + e.toString())
         return true
     }
 
-    override fun onDoubleTapEvent(e: MotionEvent?): Boolean {
+    override fun onDoubleTapEvent(e: MotionEvent?): Boolean {                 //+
         text2?.text = "Двойное нажатие и перемещение(подъем пальца и т.д.)"
-        text1!!.text = "onLongPress$e"
+        text1!!.text = ("onDoubleTapEvent :" + e.toString())
         return true
     }
 
     override fun onDown(e: MotionEvent?): Boolean {
         text2?.text = "Палец прижат к экрану"
-        text1!!.text = "onLongPress$e"
-        return true
+        text1!!.text = ("onDown :" + e.toString())
+        return false
     }
 
     override fun onShowPress(e: MotionEvent?) {
-        text2?.text = "Палец прижат к экрану"
-        text1!!.text = "onLongPress$e"
+        text2?.text = "Касание экрана пальцем без дополнительных действий"       //+
+        text1!!.text = ("onShowPress :" + e.toString())
     }
 
     override fun onSingleTapUp(e: MotionEvent?): Boolean {
-        text2?.text = "Свайп вверх"
-        text1!!.text = "onLongPress$e"
+        text2?.text = "Отжатие пальца от экрана"
+        text1!!.text = ("onSingleTapUp :" + e.toString())
         return true
     }
 
-    override fun onScroll(e1: MotionEvent?, e2: MotionEvent?, distanceX: Float, distanceY: Float): Boolean {
-        text2?.text = "Прокрутка пальцем по экрану"
-        text1!!.text = "onLongPress$e1+$e2"
+    override fun onScroll(e1: MotionEvent?, e2: MotionEvent?, distanceX: Float, distanceY: Float): Boolean {     //+
+        text2?.text = "Прокрутка пальцем по экрану(пролистывание)"
+        text1!!.text = ("onScroll :" + e1.toString() + e2.toString())
         return true
     }
 
-    override fun onLongPress(e: MotionEvent?) {
-        text2?.text = "Долгое нажатие на экран"
-        text1!!.text = "onLongPress$e"
+    override fun onLongPress(e: MotionEvent?) {       //+
+        text2?.text = "Долгое нажатие пальцем на экран"
+        text1!!.text = ("onLongPress :" + e.toString())
     }
 
-    override fun onFling(e1: MotionEvent?, e2: MotionEvent?, velocityX: Float, velocityY: Float): Boolean {
-        text2?.text = "onFling"
-        text1!!.text = "onLongPress$e1+$e2"
+    override fun onFling(e1: MotionEvent?, e2: MotionEvent?, velocityX: Float, velocityY: Float): Boolean {      //+
+        text2?.text = "Жест смахивания"
+        text1!!.text = ("onFling :" + e1.toString() + e2.toString())
         return true
     }
 
